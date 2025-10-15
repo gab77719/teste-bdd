e2f610b8a4cc6ae36341e144bdcb04cf81aefe08
Gabriel <alves.gabriel3@aluno.ifsp.edu.br>
2025-10-15 16:22:51 -0300

    alterações

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import Header from './src/components/Header'
+
+function About() {
+    return (
+        <>
+            <Header />
+            <h1>Sobre</h1>
+        </>
+    )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from './src/components/Header'
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>Início</h1>
+        </>
+    )
+}
+
+export default Home
